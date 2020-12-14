bde6ec1cad35e48668ba13cdd598f20fadb14556
YEHOR POLISHCHUK <e-dp@ukr.net>
2020-12-14 20:17:50 +0200

    config commit

---
+++ py2sqlite/__init__.py
@@ -0,0 +1,2 @@
+from . import py2sql
+from util import ModelPy2SQL
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'py2sqlite',
+  packages = ['py2sqlite'],
+  version = '0.1',
+  license='mit',
+  description = 'Package to work with SQLite',
+  author = 'Yehor Polishchuk',
+  author_email = 'e-dp@ukr.net',
+  url = 'https://github.com/ehorTL',
+  download_url = 'https://github.com/ehorTL/py2sqlite/?',
+  keywords = ['py2sqlite', 'SQLite', 'Py2SQL', 'SQL'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
